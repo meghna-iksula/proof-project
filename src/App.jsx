0d71015f2d887496a00aed841ce8f246e2206cc9
meghnaerla <meghna.e@iksula.com>
2023-06-23 15:13:23 +0530

    Added custom colors and fonts.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,21 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <p className="bg-black text-white">Hello</p>
+      <p className="font-Roboto font-light bg-green">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis,
+        quisquam quas. Necessitatibus sunt praesentium quae ratione consequatur,
+        maxime facere cum.
+      </p>
+      <p className="font-Arial">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis,
+        quisquam quas. Necessitatibus sunt praesentium quae ratione consequatur,
+        maxime facere cum.
+      </p>
+      <p className="font-Roboto-slab font-light">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis,
+        quisquam quas. Necessitatibus sunt praesentium quae ratione consequatur,
+        maxime facere cum.
+      </p>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["*"],
+  content: ["./src/**/*.jsx", "*"],
   theme: {
-    extend: {},
+    fontFamily: {
+      Arial: ["Arial", "Helvetica Neue", "Helvetica", "sans-serif"],
+      Roboto: ["Roboto", "sans-serif"],
+      "Roboto-Condensed": ["Roboto Condensed", "sans-serif"],
+      "Roboto-slab": ["Roboto Slab", "serif"],
+    },
+    extend: {
+      colors: {
+        gray: "#333",
+        green: "#00b874",
+        blue: "#0050b8",
+        "light-gray": "#00000057",
+      },
+    },
   },
   plugins: [],
 };

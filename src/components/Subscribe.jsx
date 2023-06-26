d420f6cdabfaadfce46fce8b6aa0e92ace847b54
meghnaerla <meghna.e@iksula.com>
2023-06-26 14:56:02 +0530

    Added the logic for form validation.

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -1,4 +1,66 @@
-function Subscribe() {
+import { useState } from "react";
+import validateInput from "./form-validation";
+
+const isNotEmpty = (value) => value.trim() !== "";
+const isEmail = (value) => value.includes("@");
+
+const Subscribe = () => {
+  const [enteredLocation, setLocation] = useState("");
+
+  const {
+    value: firstNameValue,
+    isValid: firstNameIsValid,
+    hasError: firstNameHasError,
+    valueChangeHandler: firstNameChangeHandler,
+    inputBlurHandler: firstNameBlurHandler,
+    reset: resetFirstName,
+  } = validateInput(isNotEmpty);
+
+  const {
+    value: lastNameValue,
+    isValid: lastNameIsValid,
+    hasError: lastNameHasError,
+    valueChangeHandler: lastNameChangeHandler,
+    inputBlurHandler: lastNameBlurHandler,
+    reset: resetLastName,
+  } = validateInput(isNotEmpty);
+
+  const {
+    value: emailValue,
+    isValid: emailIsValid,
+    hasError: emailHasError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmail,
+  } = validateInput(isEmail);
+
+  let formIsValid = false;
+
+  if (firstNameIsValid && lastNameIsValid && emailIsValid) {
+    formIsValid = true;
+  }
+
+  const locationChangeHandler = (event) => {
+    setLocation(event.target.value);
+  };
+
+  const resetLocation = () => {
+    setLocation("");
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    if (!formIsValid) {
+      return;
+    }
+
+    resetFirstName();
+    resetLastName();
+    resetEmail();
+    resetLocation();
+  };
+
   return (
     <div className="p-[20px] lg:p-[40px] lg:max-w-[630px] w-full lg:h-[90vh] mb-[100px] lg:mb-0 overflow-scroll">
       <h2 className="page-title">Subscribe to Proof</h2>
@@ -7,36 +69,84 @@ function Subscribe() {
         us your contact info and you're on your way.
       </p>
       <div>
-        <form action="#" className="text-[16px] flex flex-wrap justify-between">
-          <input
-            className="form-input w-[48%] mr-[10px] lg:mr-[15px]"
-            type="text"
-            placeholder="First Name"
-            required
-          />
-          <input
-            className="form-input w-[48%]"
-            type="text"
-            placeholder="Last Name"
-            required
-          />
+        <form
+          onSubmit={submitHandler}
+          className="text-[16px] flex flex-wrap justify-between"
+        >
+          <div className="w-full sm:w-[48%]">
+            <input
+              className={
+                firstNameHasError
+                  ? "form-input w-full mr-[10px] lg:mr-[15px] border-red-500"
+                  : "form-input w-full mr-[10px] lg:mr-[15px]"
+              }
+              type="text"
+              placeholder="First Name"
+              value={firstNameValue}
+              onChange={firstNameChangeHandler}
+              onBlur={firstNameBlurHandler}
+            />
+            {firstNameHasError && (
+              <p className="text-red-500 mb-3 capitalize">
+                Please enter your first name!
+              </p>
+            )}
+          </div>
+          <div className="w-full sm:w-[48%]">
+            <input
+              className={
+                lastNameHasError
+                  ? "form-input w-full mr-[10px] lg:mr-[15px] border-red-500"
+                  : "form-input w-full mr-[10px] lg:mr-[15px]"
+              }
+              type="text"
+              placeholder="Last Name"
+              value={lastNameValue}
+              onChange={lastNameChangeHandler}
+              onBlur={lastNameBlurHandler}
+            />
+            {lastNameHasError && (
+              <p className="text-red-500 mb-3 capitalize">
+                Please enter your last name!
+              </p>
+            )}
+          </div>
+          <div className="w-full">
+            <input
+              className={
+                emailHasError
+                  ? "form-input w-full mr-[10px] lg:mr-[15px] border-red-500"
+                  : "form-input w-full mr-[10px] lg:mr-[15px]"
+              }
+              type="text"
+              placeholder="Email Address"
+              value={emailValue}
+              onChange={emailChangeHandler}
+              onBlur={emailBlurHandler}
+            />
+            {emailHasError && (
+              <p className="text-red-500 mb-3 capitalize">
+                Please enter a valid email address!
+              </p>
+            )}
+          </div>
           <input
             className="form-input w-full"
-            type="email"
-            placeholder="Email Address"
-            required
-          />
-          <input
-            className="form-input w-full"
             type="text"
+            value={enteredLocation}
+            onChange={locationChangeHandler}
             placeholder="What's you favorite location? (Optional)"
-            required
           />
-          <button className="form-button">Subscribe</button>
+          <button
+            disabled={!formIsValid}
+            className="form-button disabled:bg-slate-500"
+          >
+            Subscribe
+          </button>
         </form>
       </div>
     </div>
   );
-}
+};
 
 export default Subscribe;
--- src/components/Suggest.jsx
+++ src/components/Suggest.jsx
@@ -1,4 +1,64 @@
+import validateInput from "./form-validation";
+
+const isNotEmpty = (value) => value.trim() !== "";
+const isEmail = (value) => value.includes("@");
+
 function Suggest() {
+  const {
+    value: fullNameValue,
+    isValid: fullNameIsValid,
+    hasError: fullNameHasError,
+    valueChangeHandler: fullNameChangeHandler,
+    inputBlurHandler: fullNameBlurHandler,
+    reset: resetFullName,
+  } = validateInput(isNotEmpty);
+
+  const {
+    value: emailValue,
+    isValid: emailIsValid,
+    hasError: emailHasError,
+    valueChangeHandler: emailChangeHandler,
+    inputBlurHandler: emailBlurHandler,
+    reset: resetEmail,
+  } = validateInput(isEmail);
+
+  const {
+    value: subjectValue,
+    isValid: subjectIsValid,
+    hasError: subjectHasError,
+    valueChangeHandler: subjectChangeHandler,
+    inputBlurHandler: subjectBlurHandler,
+    reset: resetSubject,
+  } = validateInput(isNotEmpty);
+
+  const {
+    value: storyValue,
+    isValid: storyIsValid,
+    hasError: storyHasError,
+    valueChangeHandler: storyChangeHandler,
+    inputBlurHandler: storyBlurHandler,
+    reset: resetStory,
+  } = validateInput(isNotEmpty);
+
+  let formIsValid = false;
+
+  if (fullNameIsValid && subjectIsValid && emailIsValid && storyIsValid) {
+    formIsValid = true;
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    if (!formIsValid) {
+      return;
+    }
+
+    resetFullName();
+    resetEmail();
+    resetSubject();
+    resetStory();
+  };
+
   return (
     <div className="p-[20px] lg:p-[40px] lg:max-w-[630px] w-full lg:h-[90vh] mb-[50px] lg:mb-0 overflow-scroll">
       <h2 className="page-title">Suggest an article</h2>
@@ -7,32 +67,96 @@ function Suggest() {
         awesome stuff. We all play a role people!
       </p>
       <div>
-        <form action="#" className="text-[16px] flex flex-wrap justify-between">
-          <input
-            className="form-input w-full"
-            type="text"
-            placeholder="Full Name"
-            required
-          />
-          <input
-            className="form-input w-full"
-            type="email"
-            placeholder="Email Address"
-            required
-          />
-          <input
-            className="form-input w-full"
-            type="text"
-            placeholder="What's the Subject?"
-            required
-          />
-          <textarea
-            className="form-input w-full h-[150px]"
-            maxlength="5000"
-            placeholder="What's the story?"
-            required
-          />
-          <button className="form-button">Send suggestion</button>
+        <form
+          onSubmit={submitHandler}
+          className="text-[16px] flex flex-wrap justify-between"
+        >
+          <div className="w-full">
+            <input
+              className={
+                fullNameHasError
+                  ? "form-input w-full mr-[10px] lg:mr-[15px] border-red-500"
+                  : "form-input w-full mr-[10px] lg:mr-[15px]"
+              }
+              type="text"
+              placeholder="Full Name"
+              value={fullNameValue}
+              onChange={fullNameChangeHandler}
+              onBlur={fullNameBlurHandler}
+            />
+            {fullNameHasError && (
+              <p className="text-red-500 mb-3 capitalize">
+                Please enter your first name!
+              </p>
+            )}
+          </div>
+
+          <div className="w-full">
+            <input
+              className={
+                emailHasError
+                  ? "form-input w-full mr-[10px] lg:mr-[15px] border-red-500"
+                  : "form-input w-full mr-[10px] lg:mr-[15px]"
+              }
+              type="text"
+              placeholder="Email Address"
+              value={emailValue}
+              onChange={emailChangeHandler}
+              onBlur={emailBlurHandler}
+            />
+            {emailHasError && (
+              <p className="text-red-500 mb-3 capitalize">
+                Please enter a vaild email address!
+              </p>
+            )}
+          </div>
+
+          <div className="w-full">
+            <input
+              className={
+                subjectHasError
+                  ? "form-input w-full mr-[10px] lg:mr-[15px] border-red-500"
+                  : "form-input w-full mr-[10px] lg:mr-[15px]"
+              }
+              type="text"
+              placeholder="What's the Subject?"
+              value={subjectValue}
+              onChange={subjectChangeHandler}
+              onBlur={subjectBlurHandler}
+            />
+            {subjectHasError && (
+              <p className="text-red-500 mb-3 capitalize">
+                Please enter your Subject!
+              </p>
+            )}
+          </div>
+
+          <div className="w-full">
+            <textarea
+              className={
+                storyHasError
+                  ? "form-input w-full mr-[10px] lg:mr-[15px] h-[150px] border-red-500"
+                  : "form-input w-full mr-[10px] lg:mr-[15px] h-[150px]"
+              }
+              value={storyValue}
+              onChange={storyChangeHandler}
+              onBlur={storyBlurHandler}
+              maxlength="5000"
+              placeholder="What's the story?"
+            />
+            {storyHasError && (
+              <p className="text-red-500 mb-3 capitalize">
+                Please enter your Story!
+              </p>
+            )}
+          </div>
+
+          <button
+            disabled={!formIsValid}
+            className="form-button disabled:bg-slate-500"
+          >
+            Send suggestion
+          </button>
         </form>
       </div>
     </div>
